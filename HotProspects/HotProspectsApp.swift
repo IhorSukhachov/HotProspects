0ebb0ec7784ba00496d9e188f50f2bb5909b93c3
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-08 11:23:18 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Ihor Sukhachov on 08.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
