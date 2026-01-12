b5722de374eabd7fbc2a174c598afd35e4b0983f
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-12 16:02:00 +0200

    creating prospect class

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -5,4 +5,17 @@
 //  Created by Ihor Sukhachov on 12.01.2026.
 //
 
-import Foundation
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    } 
+}
