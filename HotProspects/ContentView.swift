7bc2ef08fe7260870274c6ae8e79b280fdc931d5
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-12 15:21:41 +0200

    clearing code

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,48 +5,14 @@
 //  Created by Ihor Sukhachov on 08.01.2026.
 //
 
-import SamplePackage
+
 import SwiftUI
-import UserNotifications
+
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
     
     var body: some View {
-        VStack {
-            Button("Requst Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {success, error in
-                    if success {
-                        print("All set")
-                    } else if let error {
-                        print("there was an error: \(error.localizedDescription )")
-                    }
-                    
-                }
-            }
-        }
-        
-        VStack {
-            Button("Send Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Feed thr cat"
-                content.subtitle = "It's time to feed the cat"
-                content.sound = .default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
-        Text(results)
+
         
     }
 }
