7fd415d611a67bf3f3c45b26f02a554321d2c649
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-14 12:44:09 +0200

    preparing for qr code generation

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -5,20 +5,27 @@
 //  Created by Ihor Sukhachov on 12.01.2026.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "Anonymous@example.com"
     
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
         NavigationStack {
             Form {
                 TextField("name", text: $name)
                     .textContentType(.name)
+                    .font(.title)
                 TextField("email address", text: $emailAddress)
                     .textContentType(.emailAddress)
+                    .font(.title)
             }
+            .navigationTitle("Your Code")
            
         }
     }
