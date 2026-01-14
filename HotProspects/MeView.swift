cef0ef19e319efb639ea779f04163e0765801573
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-14 12:50:40 +0200

    written a method for generating ui iamge from data

--- HotProspects/MeView.swift
+++ HotProspects/MeView.swift
@@ -29,6 +29,15 @@ struct MeView: View {
            
         }
     }
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
 }
 
 #Preview {
