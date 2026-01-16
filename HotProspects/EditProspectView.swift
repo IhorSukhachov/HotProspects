7c111f1b1aee523aa164419bb5472930d27b9a85
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-16 11:18:03 +0200

    some styling and added save button to toolbar

--- HotProspects/EditProspectView.swift
+++ HotProspects/EditProspectView.swift
@@ -8,15 +8,50 @@ import SwiftData
 import SwiftUI
 
 struct EditProspectView: View {
+    @Environment(\.dismiss) private var dismiss
     @Bindable var prospect: Prospect
     
+    @State private var name: String
+    @State private var email: String
+    
+    init(prospect: Prospect) {
+        self.prospect = prospect
+        _name = State(initialValue: prospect.name)
+        _email = State(initialValue: prospect.emailAddress)
+    }
+    
     var body: some View {
-        TextField(prospect.name, text: $prospect.name)
-        TextField(prospect.emailAddress, text: $prospect.emailAddress)
+            NavigationStack {
+                Form {
+                    Section("Contact information") {
+                        TextField(prospect.name, text: $name)
+                            .textContentType(.name)
+                        
+                        TextField(prospect.emailAddress, text: $email)
+                            .keyboardType(.emailAddress)
+                            .textContentType(.emailAddress)
+                            .autocapitalization(.none)
+                    }
+                }
+                .navigationTitle("Edit Prospect")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Save") {
+                                    save()
+                                }
+                            }
+                        }
+            }
+        }
+    func save() {
+        prospect.name = name
+        prospect.emailAddress = email
+        dismiss()
     }
 }
 
 #Preview {
-    EditProspectView(prospect: Prospect(name: "Test", emailAddress: "Test", isContacted: false))
+    EditProspectView(prospect: Prospect(name: "Test User", emailAddress: "test@example.com",isContacted: false))
         .modelContainer(for: Prospect.self)
 }
